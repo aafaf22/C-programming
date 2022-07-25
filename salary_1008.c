ddd470cba124f3674f0a613f51d0ed9638c254bc
aafaf22 <106030441+aafaf22@users.noreply.github.com>
2022-07-25 21:51:25 +0600

    Add files via upload

---
+++ salary_1008.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a,b;
+scanf("%d %d",&a,&b);
+float x;
+scanf("%f",&x);
+printf("The employees number is :%d\n",a);
+printf("The employees salary is:U$%.2f\n",b*x);
+return 0;
+}
